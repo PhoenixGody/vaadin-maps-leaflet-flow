05d701c00dd2055caac7557c2cdbde25f243e945
Johannes Goderbauer <j.goderbauer@adito.de>
2022-06-01 09:49:26 +0200

    LeafletOptionsContainerKey: equals and hash override

--- vaadin-maps-leaflet-flow/src/main/java/software/xdev/vaadin/maps/leaflet/flow/data/options/LeafletOptionsContainerKey.java
+++ vaadin-maps-leaflet-flow/src/main/java/software/xdev/vaadin/maps/leaflet/flow/data/options/LeafletOptionsContainerKey.java
@@ -1,5 +1,7 @@
 package software.xdev.vaadin.maps.leaflet.flow.data.options;
 
+import java.util.Objects;
+
 public class LeafletOptionsContainerKey<T> {
     private final Class<T> classType;
     private final String name;
@@ -24,5 +26,20 @@ public class LeafletOptionsContainerKey<T> {
     public String toString()
     {
         return "[classType=" + getClassType() + ", name=" + getName() + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LeafletOptionsContainerKey<?> that = (LeafletOptionsContainerKey<?>) o;
+        // only name because the name should be the unique identifier
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        // only name because the name should be the unique identifier
+        return Objects.hash(name);
     }
 }
