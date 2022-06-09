414f277f4109493856b9973c36a41e9b5eee31f8
j.goderbauer <j.goderbauer@adito.de>
2022-06-09 16:08:12 +0200

    LPolyine: Make options accessable

--- vaadin-maps-leaflet-flow/src/main/java/software/xdev/vaadin/maps/leaflet/flow/data/LPolylineBase.java
+++ vaadin-maps-leaflet-flow/src/main/java/software/xdev/vaadin/maps/leaflet/flow/data/LPolylineBase.java
@@ -3,12 +3,19 @@ package software.xdev.vaadin.maps.leaflet.flow.data;
 import elemental.json.Json;
 import elemental.json.JsonObject;
 import elemental.json.JsonValue;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class LPolylineBase extends LLayer {
-    protected final LPolylineOptions properties;
+    protected final LPolylineOptions options;
 
-    public LPolylineBase(LPolylineOptions properties) {
-        this.properties = properties;
+    public LPolylineBase(LPolylineOptions options) {
+        super();
+        this.options = options;
+    }
+
+    @NotNull
+    public LPolylineOptions getOptions() {
+        return options;
     }
 
     @Override
@@ -16,7 +23,7 @@ public abstract class LPolylineBase extends LLayer {
     {
         JsonObject jsonObject = Json.createObject();
         jsonObject.put("type", Json.create("Feature"));
-        jsonObject.put("properties", this.properties.toJson());
+        jsonObject.put("properties", this.options.toJson());
         return jsonObject;
     }
 
